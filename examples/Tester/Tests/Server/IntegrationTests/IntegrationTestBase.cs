0e379f73fde2abbeb2e497f3cb5076c365e83b7b
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-14 18:02:51 +0800

    tester

--- examples/Tester/Tests/Server/IntegrationTests/IntegrationTestBase.cs
+++ examples/Tester/Tests/Server/IntegrationTests/IntegrationTestBase.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright notice and license
+#region Copyright notice and license
 
 // Copyright 2019 The gRPC Authors
 //
@@ -23,55 +23,54 @@ using NUnit.Framework;
 using Server;
 using Tests.Server.IntegrationTests.Helpers;
 
-namespace Tests.Server.IntegrationTests
+namespace Tests.Server.IntegrationTests;
+
+public class IntegrationTestBase
 {
-    public class IntegrationTestBase
+    private GrpcChannel? _channel;
+    private IDisposable? _testContext;
+
+    protected GrpcTestFixture<Startup> Fixture { get; private set; } = default!;
+
+    protected ILoggerFactory LoggerFactory => Fixture.LoggerFactory;
+
+    protected GrpcChannel Channel => _channel ??= CreateChannel();
+
+    protected GrpcChannel CreateChannel()
     {
-        private GrpcChannel? _channel;
-        private IDisposable? _testContext;
-
-        protected GrpcTestFixture<Startup> Fixture { get; private set; } = default!;
-
-        protected ILoggerFactory LoggerFactory => Fixture.LoggerFactory;
-
-        protected GrpcChannel Channel => _channel ??= CreateChannel();
-
-        protected GrpcChannel CreateChannel()
+        return GrpcChannel.ForAddress("http://localhost", new GrpcChannelOptions
         {
-            return GrpcChannel.ForAddress("http://localhost", new GrpcChannelOptions
-            {
-                LoggerFactory = LoggerFactory,
-                HttpHandler = Fixture.Handler
-            });
-        }
+            LoggerFactory = LoggerFactory,
+            HttpHandler = Fixture.Handler
+        });
+    }
 
-        protected virtual void ConfigureServices(IServiceCollection services)
-        {
-        }
+    protected virtual void ConfigureServices(IServiceCollection services)
+    {
+    }
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            Fixture = new GrpcTestFixture<Startup>(ConfigureServices);
-        }
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        Fixture = new GrpcTestFixture<Startup>(ConfigureServices);
+    }
 
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            Fixture.Dispose();
-        }
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        Fixture.Dispose();
+    }
 
-        [SetUp]
-        public void SetUp()
-        {
-            _testContext = Fixture.GetTestContext();
-        }
+    [SetUp]
+    public void SetUp()
+    {
+        _testContext = Fixture.GetTestContext();
+    }
 
-        [TearDown]
-        public void TearDown()
-        {
-            _testContext?.Dispose();
-            _channel = null;
-        }
+    [TearDown]
+    public void TearDown()
+    {
+        _testContext?.Dispose();
+        _channel = null;
     }
 }
